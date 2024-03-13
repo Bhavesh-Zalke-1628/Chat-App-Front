a913b90f71375ded27a04d5365361d9198e14943
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-03-13 17:11:14 +0530

    Add the sign in and login functionality

--- src/Component/LogIn.jsx
+++ src/Component/LogIn.jsx
@@ -1,64 +1,190 @@
-import React, { useState } from 'react'
-import { FormControl, FormLabel, VStack, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+// import React, { useState } from 'react'
+// import { FormControl, FormLabel, VStack, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
 
 
-function LogIn() {
-    const [show, setShow] = useState(false)
+// function LogIn() {
+//     const [show, setShow] = useState(false)
+//     const [email, setEmail] = useState();
+//     const [conformPassword, setConformPassword] = useState();
+
+//     const handleClick = () => setShow(!show)
+//     const postDetails = (pic) => {
+
+//     }
+
+//     const submitHandler = () => {
+
+//     }
+//     return (
+//         <VStack spacing='5px' color='black'>
+//             <FormControl id='email' isRequired>
+//                 <FormLabel>Email</FormLabel>
+//                 <Input
+//                     placeholder='Enter your email'
+//                     onChange={(e) => setEmail(e.target.value)}
+//                 />
+//             </FormControl>
+
+//             <FormControl id='password' isRequired>
+//                 <FormLabel>Password</FormLabel>
+//                 <InputGroup>
+//                     <Input
+//                         type={show ? "text" : "password"}
+//                         placeholder='Enter your password'
+//                         onChange={(e) => setPassword(e.target.value)}
+//                     />
+//                     <InputRightElement width='4.5rem'>
+//                         <Button h='1.75rem' size='sm' onClick={handleClick}>
+//                             {show ? 'Hide' : 'Show'}
+//                         </Button>
+//                     </InputRightElement>
+//                 </InputGroup>
+//             </FormControl>
+//             <Button
+//                 colorScheme='blue'
+//                 w='100%'
+//                 style={{ marginTop: 15 }}
+//                 onClick={submitHandler}
+//             >
+//                 Log In
+//             </Button>
+//             <Button
+//                 colorScheme='red'
+//                 w='100%'
+//                 style={{ marginTop: 15 }}
+//                 onClick={submitHandler}
+//             >
+//                 Guest
+//             </Button>
+//         </VStack >
+//     )
+
+// }
+
+// export default LogIn
+
+
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+// import { useHistory } from "react-router-dom";
+// import { ChatState } from "../../Context/ChatProvider";
+
+const Login = () => {
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+    const toast = useToast();
     const [email, setEmail] = useState();
-    const [conformPassword, setConformPassword] = useState();
+    const [password, setPassword] = useState();
+    const [loading, setLoading] = useState(false);
 
-    const handleClick = () => setShow(!show)
-    const postDetails = (pic) => {
+    // const history = useHistory();
+    // const { setUser } = ChatState();
 
-    }
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
 
-    const submitHandler = () => {
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
 
-    }
+            const { data } = await axios.post(
+                "http://localhost:5000/api/user/login",
+                { email, password },
+                config
+            );
+
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setUser(data);
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history.push("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
+    };
+
     return (
-        <VStack spacing='5px' color='black'>
-            <FormControl id='email' isRequired>
-                <FormLabel>Email</FormLabel>
+        <VStack spacing="10px">
+            <FormControl id="email" isRequired>
+                <FormLabel>Email Address</FormLabel>
                 <Input
-                    placeholder='Enter your email'
+                    value={email}
+                    type="email"
+                    placeholder="Enter Your Email Address"
                     onChange={(e) => setEmail(e.target.value)}
                 />
             </FormControl>
-
-            <FormControl id='password' isRequired>
+            <FormControl id="password" isRequired>
                 <FormLabel>Password</FormLabel>
-                <InputGroup>
+                <InputGroup size="md">
                     <Input
-                        type={show ? "text" : "password"}
-                        placeholder='Enter your password'
+                        value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        type={show ? "text" : "password"}
+                        placeholder="Enter password"
                     />
-                    <InputRightElement width='4.5rem'>
-                        <Button h='1.75rem' size='sm' onClick={handleClick}>
-                            {show ? 'Hide' : 'Show'}
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
                         </Button>
                     </InputRightElement>
                 </InputGroup>
             </FormControl>
             <Button
-                colorScheme='blue'
-                w='100%'
+                colorScheme="blue"
+                width="100%"
                 style={{ marginTop: 15 }}
                 onClick={submitHandler}
+                isLoading={loading}
             >
-                Log In
+                Login
             </Button>
             <Button
-                colorScheme='red'
-                w='100%'
-                style={{ marginTop: 15 }}
-                onClick={submitHandler}
+                variant="solid"
+                colorScheme="red"
+                width="100%"
+                onClick={() => {
+                    setEmail("guest@example.com");
+                    setPassword("123456");
+                }}
             >
-                Guest
+                Get Guest User Credentials
             </Button>
-        </VStack >
-    )
+        </VStack>
+    );
+};
 
-}
-
-export default LogIn
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ChakraProvider>
